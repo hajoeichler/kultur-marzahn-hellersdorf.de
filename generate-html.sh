5c89c2d888c25b08cfe5bbbe97b23076a285d1dd
Hajo Eichler <hajo.eichler@commercetools.de>
2012-09-09 14:21:22 +0200

    Add small helper to put css and js together for use in typo3 html page.

---
+++ generate-html.sh
@@ -0,0 +1,7 @@
+echo "<script src='//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js' type='text/javascript'></script>"
+echo "<style type='text/css'>"
+cat master-detail.css
+echo "</style>"
+echo "<script type='text/javascript'>"
+cat master-detail.js
+echo "</script>"
