be541afef70499a928e59cf1e13547e165a080e7
Hajo Eichler <hajo.eichler@commercetools.de>
2012-09-09 20:34:03 +0200

    Externalized data. Also create master data from data now.

--- master-detail.js
+++ master-detail.js
@@ -1,68 +1,49 @@
 masterDetails = {
+  masterQuery: "td[id='selections-master']",
   detailQuery: "td[id='selections-detail']",
   level2Query: "td[id='selections-level2']",
   runOnce: function() {
+    masterDetails.createMaster();
     masterDetails.updateDetail("Ortsteile");
+  },
+  createMaster: function() {
+    for ( property in masterDetailsData.root ) {
+      if ( masterDetailsData.root.hasOwnProperty(property) ) {
+        masterDetails.createMasterLink(property);
+      }
+    }
   },
   updateDetail: function(sel) {
     jQuery(masterDetails.detailQuery + " div").remove();
     jQuery(masterDetails.level2Query + " div").remove();
-    if (sel === "Ortsteile") {
-      masterDetails.createLinkToLevel2("Biesdorf");
-      masterDetails.createLinkToLevel2("Hellersdorf");
-      masterDetails.createLinkToLevel2("Kaulsdorf");
-      masterDetails.createLinkToLevel2("Mahlsdorf");
-      masterDetails.createLinkToLevel2("Marzahn");
-    }
-    if (sel === "Personen") {
-      masterDetails.createLinkToPage("Arndt Bause", "123");
-    }
-    if (sel === "Ereignisse") {
-      masterDetails.createLinkToLevel2("Siedlungsgeschichte");
-      masterDetails.createLinkToLevel2("Erster Weltkrieg");
-      masterDetails.createLinkToLevel2("Zweiter Weltkrieg");
-      masterDetails.createLinkToLevel2("Verfolgung im Nationalsozialismus");
-      masterDetails.createLinkToLevel2("Großsiedlungsbau");
-    }
-    if (sel === "NichtMehrVorOrt") {
-      masterDetails.createLinkToPage("Bismark Denkmal", "123");
+    for ( property in masterDetailsData.root[sel] ) {
+      if ( masterDetailsData.root[sel].hasOwnProperty(property) ) {
+        if ( typeof(masterDetailsData.root[sel][property]) === 'string' ) {
+          masterDetails.createLinkToPage(property, masterDetailsData.root[sel][property]);
+        } else {
+          masterDetails.createLinkToLevel2(property, sel);
+        }
+      }
     }
   },
-  updateLevel2: function(sel) {
+  updateLevel2: function(sel, selLevel2) {
     jQuery(masterDetails.level2Query + " div").remove();
-    if (sel === "Biesdorf") {
-      masterDetails.createLinkToPageFromLevel2("Foo", "123");
-      masterDetails.createLinkToPageFromLevel2("Bar", "123");
-    }
-    if (sel === "Hellersdorf") {
-    }
-    if (sel === "Kaulsdorf") {
-    }
-    if (sel === "Mahlsdorf") {
-    }
-    if (sel === "Marzahn") {
-    }
-    if (sel === "Siedlungsgeschichte") {
-    }
-    if (sel === "Erster Weltkrieg") {
-    }
-    if (sel === "Zweiter Weltkrieg") {
-    }
-    if (sel === "Verfolgung im Nationalsozialismus") {
-    }
-    if (sel === "Großsiedlungsbau") {
+    for ( property in masterDetailsData.root[sel][selLevel2] ) {
+      if ( masterDetailsData.root[sel][selLevel2].hasOwnProperty(property) ) {
+        masterDetails.createLinkToPageLevel2(property, masterDetailsData.root[sel][selLevel2][property]);
+      }
     }
   },
-  createLinkToLevel2: function(name, id) {
-    if (id === undefined) {
-      id = name
-    }
-    masterDetails.createLink(name, id, "updateLevel2", masterDetails.detailQuery); 
+  createMasterLink: function(property) {
+    masterDetails.createLink(property, property, "updateDetail", masterDetails.masterQuery);
+  },
+  createLinkToLevel2: function(name, sel) {
+    masterDetails.createLink(name, sel + '", "' + name, "updateLevel2", masterDetails.detailQuery);
   },
   createLinkToPage: function(name, id) {
     masterDetails.createLink(name, id, "openPageWithId", masterDetails.detailQuery);
   },
-  createLinkToPageFromLevel2: function(name, id) {
+  createLinkToPageLevel2: function(name, id) {
     masterDetails.createLink(name, id, "openPageWithId", masterDetails.level2Query);
   },
   createLink: function(name, id, mdFunction, baseQuery) {
