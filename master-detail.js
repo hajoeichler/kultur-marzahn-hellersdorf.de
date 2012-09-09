09e2a35e6bad2866d61973197b10cdafe786f0e2
Hajo Eichler <hajo.eichler@commercetools.de>
2012-09-09 17:30:17 +0200

    Introduced 2nd level. Add some real data.

--- master-detail.js
+++ master-detail.js
@@ -1,27 +1,77 @@
 masterDetails = {
-  baseQuery: "td[id='selections-detail']",
+  detailQuery: "td[id='selections-detail']",
+  level2Query: "td[id='selections-level2']",
   runOnce: function() {
-    masterDetails.updateDetail("Numbers");
+    masterDetails.updateDetail("Ortsteile");
   },
   updateDetail: function(sel) {
-    jQuery(masterDetails.baseQuery + " div").remove();
-    if (sel === "Numbers") {
-      masterDetails.createElement("1", "1");
-      masterDetails.createElement("2", "2");
-      masterDetails.createElement("3", "3");
+    jQuery(masterDetails.detailQuery + " div").remove();
+    jQuery(masterDetails.level2Query + " div").remove();
+    if (sel === "Ortsteile") {
+      masterDetails.createLinkToLevel2("Biesdorf");
+      masterDetails.createLinkToLevel2("Hellersdorf");
+      masterDetails.createLinkToLevel2("Kaulsdorf");
+      masterDetails.createLinkToLevel2("Mahlsdorf");
+      masterDetails.createLinkToLevel2("Marzahn");
     }
-    if (sel === "Chars") {
-      masterDetails.createElement("A", "4");
-      masterDetails.createElement("B", "5");
+    if (sel === "Personen") {
+      masterDetails.createLinkToPage("Arndt Bause", "123");
+    }
+    if (sel === "Ereignisse") {
+      masterDetails.createLinkToLevel2("Siedlungsgeschichte");
+      masterDetails.createLinkToLevel2("Erster Weltkrieg");
+      masterDetails.createLinkToLevel2("Zweiter Weltkrieg");
+      masterDetails.createLinkToLevel2("Verfolgung im Nationalsozialismus");
+      masterDetails.createLinkToLevel2("Großsiedlungsbau");
+    }
+    if (sel === "NichtMehrVorOrt") {
+      masterDetails.createLinkToPage("Bismark Denkmal", "123");
     }
   },
-  createElement: function(name, id) {
+  updateLevel2: function(sel) {
+    jQuery(masterDetails.level2Query + " div").remove();
+    if (sel === "Biesdorf") {
+      masterDetails.createLinkToPageFromLevel2("Foo", "123");
+      masterDetails.createLinkToPageFromLevel2("Bar", "123");
+    }
+    if (sel === "Hellersdorf") {
+    }
+    if (sel === "Kaulsdorf") {
+    }
+    if (sel === "Mahlsdorf") {
+    }
+    if (sel === "Marzahn") {
+    }
+    if (sel === "Siedlungsgeschichte") {
+    }
+    if (sel === "Erster Weltkrieg") {
+    }
+    if (sel === "Zweiter Weltkrieg") {
+    }
+    if (sel === "Verfolgung im Nationalsozialismus") {
+    }
+    if (sel === "Großsiedlungsbau") {
+    }
+  },
+  createLinkToLevel2: function(name, id) {
+    if (id === undefined) {
+      id = name
+    }
+    masterDetails.createLink(name, id, "updateLevel2", masterDetails.detailQuery); 
+  },
+  createLinkToPage: function(name, id) {
+    masterDetails.createLink(name, id, "openPageWithId", masterDetails.detailQuery);
+  },
+  createLinkToPageFromLevel2: function(name, id) {
+    masterDetails.createLink(name, id, "openPageWithId", masterDetails.level2Query);
+  },
+  createLink: function(name, id, mdFunction, baseQuery) {
     var div = document.createElement('div');
     var ahref = document.createElement('a');
     ahref.textContent = name;
-    ahref.href = 'javascript:masterDetails.openPageWithId("' + id +'");';
     div.appendChild(ahref);
-    jQuery(masterDetails.baseQuery).append(div);
+    ahref.href = 'javascript:masterDetails.' + mdFunction + '("' + id +'");';
+    jQuery(baseQuery).append(div);
   },
   openPageWithId: function(pageId) {
     if (pageId !== '') {
